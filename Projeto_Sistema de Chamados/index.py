0956a60e6e5bb77e751d144a98d8498082108e92
adriano020 <adriano020@gmail.com>
2026-05-18 20:44:31 -0300

    mm

--- Projeto_Sistema de Chamados/index.py
+++ Projeto_Sistema de Chamados/index.py
@@ -9,7 +9,7 @@ if "chamados" not in st.session_state:
 #Abrir chamado
 st.subheader("Abrir Chamado")
 titulo=st.text_input("Título do Chamado")
-descricao=st.text_area("Descriçao de Serviço")
+descricao=st.text_area("Descrição de Serviço")
 
 #Botao
 if st.button("Abrir Chamado"):
@@ -21,3 +21,25 @@ if st.button("Abrir Chamado"):
         }
         st.session_state.chamados.append(chamado)
         st.success("Chamado aberto com Sucesso")
+st.subheader("lista de chamados")
+if len(st.session_state.chamados) == 0:
+    st.warning("Nenhum Chamado Aberto")
+else:
+    for i, chamado in enumerate(st.session_state.chamados):
+        st.write(f"{chamado['titulo']}")
+        st.write(f"Descrição: {chamado['descricao']}")
+        st.write(f"Status: {chamado['status']}")
+
+        #Alterar espaço
+        novo_status = st. selectbox(
+            "Alterar Status",
+            ["Aberto", "Em Andamento", "Finalizado"],
+            key=f"status{i}"
+        )
+
+        #Botao para Alterar
+        if st.button("Atualizar Status", key=f"btn{i}"):
+            st.session_state.chamados[i]["status"] = novo_status
+            st.success("Status Atualizado!")
+            st.rerun()
+        st.divider()
